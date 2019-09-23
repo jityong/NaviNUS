0a14d4dc88cf0cbfa76181d5f15406c765a8842e
Lee Ee Jian <e0309900@u.nus.edu>
2019-09-23 12:24:13 +0800

    added 2D Map feature

--- Assets/Scripts/NavController.cs
+++ Assets/Scripts/NavController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System;
+using UnityEngine.UI;
 
 public class NavController : MonoBehaviour {
-
+    
     public AStar AStar;
     private Transform destination;
     private bool _initialized = false;
@@ -13,7 +15,14 @@ public class NavController : MonoBehaviour {
     private int currNodeIndex = 0;
     private float maxDistance = 1.1f;
 
+    [SerializeField] private Sprite circleSprite; //added
+    public Sprite targetSprite; //added
+    public GameObject graphPanel;
+    public GameObject graphContainerPanel; //added
+    private RectTransform graphContainer; //added
+
     private void Start() {
+        graphContainer = graphContainerPanel.GetComponent<RectTransform>();
 #if UNITY_EDITOR
         InitializeNavigation();
 #endif
@@ -84,7 +93,111 @@ public class NavController : MonoBehaviour {
             //activate first node
             path[0].Activate(true);
             _initializedComplete = true;
+
+            // Populate windowGraph
+            showGraphWithTargetAndAllNodes(path, target);
+
+            // setPanel to be active
+            graphPanel.SetActive(true);
         }
+    }
+
+    // Takes in the path and the Dest Node. Creates Sprite for them and draws path.
+    private void showGraphWithTargetAndAllNodes(List<Node> path, Node target)
+    {
+        int totalNumberOfNodes = path.Count + 1; //because of target ndoe
+        float graphHeight = graphContainer.sizeDelta.y;
+        float graphWidth = graphContainer.sizeDelta.x;
+        float[] yArr = new float[totalNumberOfNodes]; //for tgt Node
+        float[] xArr = new float[totalNumberOfNodes]; //for tgt Node
+
+        for (int i = 0; i < totalNumberOfNodes; i++) //-1 becasue target is already coded in.
+        {
+            if (i == (totalNumberOfNodes - 1))
+            {
+                yArr[totalNumberOfNodes] = (float)target.pos.y;
+                xArr[totalNumberOfNodes] = (float)target.pos.x;
+            }
+            else
+            {
+                yArr[i] = (float)path[i].pos.y;
+                xArr[i] = (float)path[i].pos.x;
+            }
+        }
+        float yMaximum = yArr.Max();
+        float yMinimum = yArr.Min();
+        float xMaximum = xArr.Max();
+        float xMinimum = xArr.Min();
+
+        GameObject lastGameObject = null;
+        for (int i = 0; i < totalNumberOfNodes; i++)
+        {
+            float xPosition = (xArr[i] - xMinimum) / (xMaximum - xMinimum) * graphWidth;
+            float yPosition = (yArr[i] - yMinimum) / (yMaximum - yMinimum) * graphHeight;
+            if (i == (totalNumberOfNodes - 1)) // for the last, Dest Node
+            {
+                GameObject targetGameObject = CreateTarget(new Vector2(xPosition, yPosition));
+                joinLinesForPath(lastGameObject.GetComponent<RectTransform>().anchoredPosition,
+                                             targetGameObject.GetComponent<RectTransform>().anchoredPosition);
+            }
+            else
+            {
+                GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
+                if (lastGameObject != null)
+                {
+                    joinLinesForPath(lastGameObject.GetComponent<RectTransform>().anchoredPosition,
+                                                 circleGameObject.GetComponent<RectTransform>().anchoredPosition);
+                }
+                lastGameObject = circleGameObject;
+            }
+        }
+
+    }
+
+    //Creates and returns the Target Sprite with the relative position calculated
+    private GameObject CreateTarget(Vector2 anchoredPosition)
+    {
+        GameObject gameObject = new GameObject("target", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = targetSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        return gameObject;
+    }
+
+    // Creates and returns the Circle Sprite with relative position calculated
+    private GameObject CreateCircle(Vector2 anchoredPosition)
+    {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = anchoredPosition;
+        rectTransform.sizeDelta = new Vector2(11, 11);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        return gameObject;
+    }
+
+    // Calculates and draws the correct Line angle to connect Nodes.
+    private void joinLinesForPath(Vector2 dotPositionA, Vector2 dotPositionB)
+    {
+        GameObject gameObject = new GameObject("Lines", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, .5f);
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+        rectTransform.anchorMin = new Vector2(0, 0);
+        rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.sizeDelta = new Vector2(distance , 3f); //?
+        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, 
+                    (float) Math.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -95,4 +208,5 @@ public class NavController : MonoBehaviour {
             }
         }
     }
+    
 }
---
+++ Assets/Scripts/Window_Graph.cs
@@ -0,0 +1,94 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class Window_Graph : MonoBehaviour
+//{
+//    [SerializeField] private Sprite circleSprite;
+//    private RectTransform graphContainer;
+
+//    List<Vector2> valueList = new List<Vector2>() { new Vector2((float)-1.88385177, -2),
+//            new Vector2(-2,-5),
+//            new Vector2(3,2) };
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        graphContainer = GetComponent<RectTransform>();
+//        showGraph(valueList);
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+
+//    void Awake()
+//    {
+//    }
+
+//    private GameObject CreateCircle(Vector2 anchoredPosition)
+//    {
+//        GameObject gameObject = new GameObject("circle", typeof(Image));
+//        gameObject.transform.SetParent(graphContainer, false);
+//        gameObject.GetComponent<Image>().sprite = circleSprite;
+//        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+//        rectTransform.anchoredPosition = anchoredPosition;
+//        rectTransform.sizeDelta = new Vector2(11, 11);
+//        rectTransform.anchorMin = new Vector2(0, 0);
+//        rectTransform.anchorMax = new Vector2(0, 0);
+//        return gameObject;
+//    }
+
+//    private void showGraph(List<Vector2> valueList)
+//    {
+//        float graphHeight = graphContainer.sizeDelta.y;
+//        float graphWidth = graphContainer.sizeDelta.x;
+//        float[] yArr = new float[valueList.Count];
+//        float[] xArr = new float[valueList.Count];
+//        for (int i = 0; i < valueList.Count; i++)
+//        {
+//            yArr[i] = valueList[i].y; // TODO how to extract y value from the Node 
+//            xArr[i] = valueList[i].x; // TODO how to extract x vaue from the Node
+//        }
+//        float yMaximum = yArr.Max();
+//        float yMinimum = yArr.Min();
+//        float xMaximum = xArr.Max();
+//        float xMinimum = xArr.Min();
+
+//        GameObject lastCircleGameObject = null;
+//        for (int i = 0; i < valueList.Count; i++)
+//        {
+//            float xPosition = (xArr[i] - xMinimum) / (xMaximum - xMinimum) * graphWidth;
+//            float yPosition = (yArr[i] - yMinimum) / (yMaximum - yMinimum) * graphHeight;
+//            GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
+//            if (lastCircleGameObject != null)
+//            {
+//                joinLinesForPath(lastCircleGameObject.GetComponent<RectTransform>().anchoredPosition,
+//                                             circleGameObject.GetComponent<RectTransform>().anchoredPosition);
+//            }
+//            lastCircleGameObject = circleGameObject;
+//        }
+//    }
+
+//    private void joinLinesForPath(Vector2 dotPositionA, Vector2 dotPositionB)
+//    {
+//        GameObject gameObject = new GameObject("Lines", typeof(Image));
+//        gameObject.transform.SetParent(graphContainer, false);
+//        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, .5f);
+//        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+//        Vector2 dir = (dotPositionB - dotPositionA).normalized;
+//        float distance = Vector2.Distance(dotPositionA, dotPositionB);
+//        rectTransform.anchorMin = new Vector2(0, 0);
+//        rectTransform.anchorMax = new Vector2(0, 0);
+//        rectTransform.sizeDelta = new Vector2(distance, 3f); //?
+//        rectTransform.anchoredPosition = dotPositionA + dir * distance * .5f;
+//        rectTransform.localEulerAngles = new Vector3(0, 0,
+//                    (float)Math.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+
+//    }
+//}
