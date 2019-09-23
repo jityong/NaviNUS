5dc041fd19b02838938814d33e984bc3b3a8f354
Jit Yong <jityong1997@hotmail.com>
2019-09-23 12:01:24 +0800

    Add map button logic

--- Assets/Scripts/PopulateMaps.cs
+++ Assets/Scripts/PopulateMaps.cs
@@ -11,9 +11,11 @@ using Newtonsoft.Json;
 
 public class PopulateMaps : MonoBehaviour, PlacenoteListener {
 
+
     //change map name to "userinput" (hardcode first) 
     private string MAP_NAME = "";
-
+	private List<string> buildings = new List<string>();
+	private List<string> floors = new List<string>();
     private UnityARSessionNativeInterface mSession;
     private bool mARInit = false;
 
@@ -28,73 +30,131 @@ public class PopulateMaps : MonoBehaviour, PlacenoteListener {
         }
     }
 
-    void handleUserInput(String input)
+   public void getBuildings()
+	{
+		//List<string> buildingsHere = new List<string>();
+		LibPlacenote.Instance.ListMaps((LibPlacenote.MapInfo[] obj) => {
+			foreach (LibPlacenote.MapInfo map in obj)
+			{
+				if (map.metadata.name != null)
+				{
+					Debug.Log(map.metadata.name);
+					string[] buildingNameTemp = map.metadata.name.Split(new char[] { '/' });
+					string buildingName = buildingNameTemp[0];
+					//string buildingName = MAP_NAME.Substring(0, MAP_NAME.Length - 1);
+					//Debug.Log(buildingName + " line 45");
+					if (!buildings.Contains(buildingName))
+					{
+						buildings.Add(buildingName);
+						Debug.Log("buildingName:" + buildingName);
+					}
+				}
+				Debug.Log("test");
+			}
+		});
+		Debug.Log("buildings: " + string.Join(", ", buildings.ToArray()));
+	}
+
+	public void getFloors()
+	{
+		//List<string> buildingsHere = new List<string>();
+		LibPlacenote.Instance.ListMaps((LibPlacenote.MapInfo[] obj) => {
+			foreach (LibPlacenote.MapInfo map in obj)
+			{
+				if (map.metadata.name != null && MAP_NAME != null)
+				{
+					Debug.Log(map.metadata.name);
+                    Debug.Log(MAP_NAME);
+					string[] buildingNameTemp = map.metadata.name.Split(new char[] { '/' });
+					string buildingName = buildingNameTemp[0];
+					string floorNum = buildingNameTemp[1];
+					string[] currentBuildingArr = MAP_NAME.Split(new char[] { '/' });
+					string currentBuilding = currentBuildingArr[0];
+					//string buildingName = MAP_NAME.Substring(0, MAP_NAME.Length - 1);
+					//Debug.Log(buildingName + " line 45");
+					if (currentBuilding == buildingName && !floors.Contains(floorNum))
+					{
+						floors.Add(floorNum);
+						Debug.Log("floorNum:" + floorNum);
+					}
+				}
+				Debug.Log("test");
+			}
+		});
+		Debug.Log("floors: " + string.Join(", ", floors.ToArray()));
+	}
+
+
+	public void handleUserInput(String input)
     {
-        MAP_NAME = MAP_NAME + input;
+        MAP_NAME += input + "/";
         Debug.Log("userinput: " + MAP_NAME);
+		getFloors();
+		Debug.Log("after findmap: 36");
     }
+    
+    //// Use this for initialization
+    //void Start() {
+    //    Input.location.Start();
 
-    // Use this for initialization
-    void Start() {
-        Input.location.Start();
-
-        //mSession = UnityARSessionNativeInterface.GetARSessionNativeInterface();
-        FeaturesVisualizer.EnablePointcloud();
-        LibPlacenote.Instance.RegisterListener(this);
-    }
+    //    //mSession = UnityARSessionNativeInterface.GetARSessionNativeInterface();
+    //    FeaturesVisualizer.EnablePointcloud();
+    //    LibPlacenote.Instance.RegisterListener(this);
+    //}
 
     void OnDisable() {
     }
 
     // Update is called once per frame
-    void Update() {
-        if (!mARInit && LibPlacenote.Instance.Initialized())
-        {
-            Debug.Log("Ready to Start!");
-            mARInit = true;
 
-            // Load Map
-            FindMap();
-        }
-    }
 
     
     void FindMap() {
         //get metadata
         LibPlacenote.Instance.SearchMaps(MAP_NAME, (LibPlacenote.MapInfo[] obj) => {
+			Debug.Log("findmap current obj: " + obj);
             foreach (LibPlacenote.MapInfo map in obj) {
-                //change to contains
-                if (map.metadata.name.Contains(MAP_NAME)) {
+				Debug.Log("findmap current map: " + map);
+				//change to contains
+				if (map.metadata.name == MAP_NAME) {
                     Debug.Log("IN findmap method:" + MAP_NAME);
                     mSelectedMapInfo = map;
                     Debug.Log("FOUND MAP: " + mSelectedMapInfo.placeId);
                     LoadMap();
                     return;
                 }
+				Debug.Log("cannot find in findmap:" + MAP_NAME);
             }
         });
     }
 
-    void LoadMap() {
-        ConfigureSession(false);
+	void LoadMap()
+	{
+		ConfigureSession(false);
 
-        LibPlacenote.Instance.LoadMap(mSelectedMapInfo.placeId,
-            (completed, faulted, percentage) => {
-                if (completed) {
-                    Debug.Log("Loaded ID: " + mSelectedMapInfo.placeId + "...Starting session");
+		LibPlacenote.Instance.LoadMap(mSelectedMapInfo.placeId,
+			(completed, faulted, percentage) =>
+			{
+				if (completed)
+				{
+					Debug.Log("Loaded ID: " + mSelectedMapInfo.placeId + "...Starting session");
 
-                    LibPlacenote.Instance.StartSession();
+					LibPlacenote.Instance.StartSession();
 
-                } else if (faulted) {
-                    Debug.Log("Failed to load ID: " + mSelectedMapInfo.placeId);
-                } else {
-                    Debug.Log("Map Download: " + percentage.ToString("F2") + "/1.0");
-                }
-            }
-        );
-    }
+				}
+				else if (faulted)
+				{
+					Debug.Log("Failed to load ID: " + mSelectedMapInfo.placeId);
+				}
+				else
+				{
+					Debug.Log("Map Download: " + percentage.ToString("F2") + "/1.0");
+				}
+			}
+		);
+	}
 
-    private void StartARKit() {
+	private void StartARKit() {
         Debug.Log("Initializing ARKit");
         Application.targetFrameRate = 60;
         ConfigureSession(false);
@@ -125,8 +185,8 @@ public class PopulateMaps : MonoBehaviour, PlacenoteListener {
             Debug.Log("Searching for position lock");
         } else if (currStatus == LibPlacenote.MappingStatus.WAITING) {
             if (GetComponent<CustomShapeManager>().shapeObjList.Count != 0) {
-                //GetComponent<CustomShapeManager>().ClearShapes();
-            }
+				//GetComponent<CustomShapeManager>().ClearShapes();
+			}
         }
     }
 }
--- Assets/Scripts/ReadMap.cs
+++ Assets/Scripts/ReadMap.cs
@@ -11,11 +11,14 @@ using Newtonsoft.Json;
 
 public class ReadMap : MonoBehaviour, PlacenoteListener {
 
-    //change map name to "userinput" (hardcode first) 
-    private const string MAP_NAME = "GenericMap";
+    private List<string> buildings = new List<string>();
+    private List<string> floors = new List<string>();
+    private List<string> destinations = new List<string>();
+    private string mapName = "";
 
     private UnityARSessionNativeInterface mSession;
     private bool mARInit = false;
+    private bool isNavReady = false; 
 
     private LibPlacenote.MapMetadataSettable mCurrMapDetails;
 
@@ -28,6 +31,9 @@ public class ReadMap : MonoBehaviour, PlacenoteListener {
         }
     }
 
+    public GameObject navigationPanel;
+    public GameObject navigationPanelText;
+
     // Use this for initialization
     void Start() {
         Input.location.Start();
@@ -36,6 +42,9 @@ public class ReadMap : MonoBehaviour, PlacenoteListener {
         StartARKit();
         FeaturesVisualizer.EnablePointcloud();
         LibPlacenote.Instance.RegisterListener(this);
+        //getBuildings();
+        //Debug.Log("After get building in Start()");
+
     }
 
     void OnDisable() {
@@ -45,21 +54,118 @@ public class ReadMap : MonoBehaviour, PlacenoteListener {
     void Update() {
         if (!mARInit && LibPlacenote.Instance.Initialized())
         {
-            Debug.Log("Ready to Start!");
-            mARInit = true;
+            getBuildings();
+            Debug.Log("After get building in Start()");
 
-            // Load Map
+            Debug.Log("Ready to Start!!!!!!2");
+            mARInit = true;            
+        }
+
+        if (isNavReady)
+        {            
             FindMap();
         }
     }
 
-    
+    public void getBuildings()
+    {        
+        LibPlacenote.Instance.ListMaps((LibPlacenote.MapInfo[] obj) => {
+            foreach (LibPlacenote.MapInfo map in obj)
+            {
+                if (map.metadata.name != null)
+                {
+                    Debug.Log(map.metadata.name);
+                    string[] buildingNameTemp = map.metadata.name.Split(new char[] { '/' });
+                    string buildingName = buildingNameTemp[0];
+                    if (!buildings.Contains(buildingName))
+                    {
+                        buildings.Add(buildingName);
+                        Debug.Log("buildingName:" + buildingName);
+                    }
+                }                
+            }
+            Debug.Log("Exit getBuildings()");
+        });
+        Debug.Log("buildings: " + string.Join(", ", buildings.ToArray()));
+    }
+
+    public void getFloors()
+    {
+        //List<string> buildingsHere = new List<string>();
+        LibPlacenote.Instance.ListMaps((LibPlacenote.MapInfo[] obj) => {
+            foreach (LibPlacenote.MapInfo map in obj)
+            {
+                if (map.metadata.name != null && mapName != null)
+                {
+                    Debug.Log("metadataname:" + map.metadata.name);
+                    Debug.Log(mapName);
+                    string[] buildingNameTemp = map.metadata.name.Split(new char[] { '/' });
+
+                    if (buildingNameTemp.Length > 1)
+                    {
+                        string buildingName = buildingNameTemp[0];
+                        string floorNum = buildingNameTemp[1];
+                        string[] currentBuildingArr = mapName.Split(new char[] { '/' });
+                        string currentBuilding = currentBuildingArr[0];
+                        if (currentBuilding == buildingName && !floors.Contains(floorNum))
+                        {
+                            floors.Add(floorNum);
+                            Debug.Log("floorNum:" + floorNum);
+                        }
+                    }
+                }                
+            }
+            Debug.Log("exit getFloors()");
+        });
+        Debug.Log("floors: " + string.Join(", ", floors.ToArray()));
+    }
+    public void getDestination()
+    {
+        //List<string> buildingsHere = new List<string>();
+        LibPlacenote.Instance.ListMaps((LibPlacenote.MapInfo[] obj) => {
+            foreach (LibPlacenote.MapInfo map in obj)
+            {
+                if (map.metadata.name != null && mapName != null)
+                {
+                    Debug.Log("metadataname:" + map.metadata.name);
+                    Debug.Log(mapName);
+                    string[] buildingNameTemp = map.metadata.name.Split(new char[] { '/' });
+
+                    if (buildingNameTemp.Length > 2)
+                    {
+                        string buildingName = buildingNameTemp[0];
+                        string floorNum = buildingNameTemp[1];
+                        string destination = buildingNameTemp[2];
+                        string[] currentBuildingArr = mapName.Split(new char[] { '/' });
+                        string currentBuilding = currentBuildingArr[0];
+                        string currentFloor = currentBuildingArr[1];
+                        if (currentBuilding == buildingName && currentFloor == floorNum && !destinations.Contains(destination) )
+                        {
+                            destinations.Add(destination);
+                            Debug.Log("destination :" + destination);
+                        }
+                    }
+                }
+            }
+            Debug.Log("exit getDestination()");
+
+        });
+        Debug.Log("destinations: " + string.Join(", ", destinations.ToArray()));
+    }
+
+
+    public void handleUserInput(String input)
+    {
+        mapName += input + "/";
+        Debug.Log("userinput: " + mapName);
+    }
+
     void FindMap() {
         //get metadata
-        LibPlacenote.Instance.SearchMaps(MAP_NAME, (LibPlacenote.MapInfo[] obj) => {
+        LibPlacenote.Instance.SearchMaps(mapName, (LibPlacenote.MapInfo[] obj) => {
             foreach (LibPlacenote.MapInfo map in obj) {
-                //change to contains
-                if (map.metadata.name.MAP_NAME) {
+                //change to contains                
+                if (map.metadata.name == mapName) {
                     mSelectedMapInfo = map;
                     Debug.Log("FOUND MAP: " + mSelectedMapInfo.placeId);
                     LoadMap();
@@ -110,17 +216,27 @@ public class ReadMap : MonoBehaviour, PlacenoteListener {
     public void OnStatusChange(LibPlacenote.MappingStatus prevStatus, LibPlacenote.MappingStatus currStatus) {
         Debug.Log("prevStatus: " + prevStatus.ToString() + " currStatus: " + currStatus.ToString());
         if (currStatus == LibPlacenote.MappingStatus.RUNNING && prevStatus == LibPlacenote.MappingStatus.LOST) {
-            Debug.Log("Localized: " + mSelectedMapInfo.metadata.name);
+            //Debug.Log("Localized: " + mSelectedMapInfo.metadata.name);
+            string[] mapNameArray = mapName.Split('/');
+            navigationPanelText.GetComponent<Text>().text =
+                "Navigating to " + mapNameArray[0] + ", Floor " + mapNameArray[1] + ", " + mapNameArray[2];
             GetComponent<CustomShapeManager>().LoadShapesJSON(mSelectedMapInfo.metadata.userdata);
             FeaturesVisualizer.DisablePointcloud();
         } else if (currStatus == LibPlacenote.MappingStatus.RUNNING && prevStatus == LibPlacenote.MappingStatus.WAITING) {
             Debug.Log("Mapping");
         } else if (currStatus == LibPlacenote.MappingStatus.LOST) {
-            Debug.Log("Searching for position lock");
+            navigationPanelText.GetComponent<Text>().text = "Locating...";
+            //Debug.Log("Searching for position lock");
         } else if (currStatus == LibPlacenote.MappingStatus.WAITING) {
             if (GetComponent<CustomShapeManager>().shapeObjList.Count != 0) {
                 //GetComponent<CustomShapeManager>().ClearShapes();
             }
         }
+    }
+
+    public void SetMapName(String mapName) {
+        this.mapName += mapName;
+        isNavReady = true;
+        navigationPanel.SetActive(true);
     }
 }
